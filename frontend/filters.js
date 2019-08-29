2eaae4b1bb5d1debd67c1ef89b147f7e65c45093
huangbuchao <huangbuchao@wanduoduo.com>
2019-08-29 15:55:34 +0800

    :turtle:add: filters

---
+++ frontend/filters.js
@@ -0,0 +1,6 @@
+export function formatTime(timestamp, format) {
+  const date = new Date(timestamp);
+  return `${date.toString().match(/\d\d:\d\d:\d\d/)[0]}${
+    format === "ms" ? "." + date.getMilliseconds() : ""
+  }`;
+}
