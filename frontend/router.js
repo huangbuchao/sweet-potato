24573f9de4286aa6f9d14074b8907a8aecc2fa77
huangbuchao <huangbuchao@wanduoduo.com>
2019-09-01 14:11:30 +0800

    :turtle:init views and router

--- frontend/router.js
+++ frontend/router.js
@@ -5,9 +5,73 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 
+import Components from "./views/components/Components.vue";
+import Events from "./views/events/Events.vue";
+import Stats from "./views/stats/Stats.vue";
+import Store from "./views/store/Store.vue";
+import Setting from "./views/settings/Setting.vue";
+import Router from "./views/router/Router.vue";
+import Routing from "./views/routing/Routing.vue";
+
 Vue.use(VueRouter);
 
-const routes = {};
+const routes = [
+  {
+    path: '/',
+    redirect: { name: 'components' }
+  },
+  {
+    path: '/components',
+    name: 'components',
+    component: Components
+  },
+  {
+    path: '/store',
+    name: 'store',
+    component: Store
+  },
+  {
+    path: '/events',
+    name: 'events',
+    component: Events
+  },
+  {
+    path: '/router',
+    name: 'router',
+    component: Router
+  },
+  {
+    path: '/routing',
+    name: 'routing',
+    component: Routing
+  },
+  {
+    path: '/stats',
+    component: Stats,
+    name: 'stats',
+    children: [
+      {
+        path: 'fps',
+        name: 'fps',
+        component: Stats
+      },
+      {
+        path: 'component-render',
+        name: 'component-render',
+        component: Stats
+      }
+    ]
+  },
+  {
+    path: '/setting',
+    name: 'setting',
+    component: Setting
+  },
+  {
+    path: '*',
+    redirect: '/'
+  }
+];
 
 const router = new VueRouter({
   routes
