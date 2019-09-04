c5138864c8d823aed1aede3b81be3897438b52c3
huangbuchao <huangbuchao@wanduoduo.com>
2019-09-04 17:47:06 +0800

    :dog:update a little

--- agent/agent.js
+++ agent/agent.js
@@ -1,6 +1,19 @@
 /**
  * @flow
  */
-export default class Agent {
-
+export default function Agent() {
+  this.compiler = {};
+  this.translater = {
+    format: '',
+    master: null,
+    slave: null
+  };
 }
+
+Agent.prototype.run = function() {
+
+};
+
+Agent.prototype.translate = function() {
+
+};
