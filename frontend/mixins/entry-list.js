addaa8aee0d62ee08e290ff290a6c1928eaa5b1d
huangbuchao <huangbuchao@wanduoduo.com>
2019-09-20 11:26:26 +0800

    :honeybee:add entry-list mixins

--- frontend/mixins/entry-list.js
+++ frontend/mixins/entry-list.js
@@ -2,3 +2,39 @@
  * @flow
  */
 
+import debounce from "lodash.debounce";
+
+export default function({ indexOffset = 0 } = {}) {
+  return {
+    watch: {
+      inspectedIndex(value) {
+        this.scrollIntoInspected(value);
+      }
+    },
+
+    mounted() {
+      requestAnimationFrame(() => {
+        if (this.inspectedIndex) this.scrollIntoInspected(this.inspectedIndex);
+      });
+    },
+    //not use arrow function
+    methods: {
+      scrollIntoInspected: debounce(function(index) {
+        index += indexOffset;
+        this.$nextTick(() => {
+          const scroller =
+            this.$globalRefs.leftRecycleList || this.$globalRefs.leftScroll;
+          const parentHeight = scroller.offsetHeight;
+          const height = this.highDensity ? 22 : 34;
+          const top = index * height;
+          const scrollTop = scroller.scrollTop;
+          if (top < scrollTop) {
+            scroller.scrollTop = top;
+          } else if (top + height > scrollTop + parentHeight) {
+            scroller.scrollTop = top + height - parentHeight;
+          }
+        });
+      }, 30)
+    }
+  };
+}
