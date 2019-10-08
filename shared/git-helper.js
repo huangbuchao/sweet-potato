4f896fbb2c8a553f474e64a0ddf106c54163cc86
huangbuchao <huangbuchao@wanduoduo.com>
2019-10-08 20:34:04 +0800

    :snail::deciduous_tree:chore: update

--- shared/git-helper.js
+++ shared/git-helper.js
@@ -3,7 +3,26 @@
 const { execSync } = require("child_process");
 
 const args = process.argv.slice(2);
-console.log(args);
-execSync("git add .");
-execSync(`git commit -m ":snail::seedling:chore update"`);
-execSync("git push");
+const commitMessagePrefix = {
+  default: ":flower::honeybee:chore: ",
+  frontend: ":snail::deciduous_tree:chore: ",
+  backend: ":seedling::cat2:chore: ",
+  shared: ":turtle::shell:chore: ",
+  agent: ":earth_asia::dragon:chore: ",
+  shells: ":mushroom::jack_o_lantern:chore: ",
+};
+
+function push(arg1 = "default", arg2 = "update a little") {
+  console.log(arg1, arg2);
+  try {
+    console.log('pendding');
+    execSync("git add .");
+    execSync(`git commit -m "${commitMessagePrefix[arg1]}${arg2}"`);
+    execSync("git push");
+    console.log('push done');
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+push(...args);
