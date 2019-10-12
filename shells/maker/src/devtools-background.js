fbab3d111d482b6de1daab378f7e8e9f21c02c98
huangbuchao <huangbuchao@wanduoduo.com>
2019-10-13 01:33:28 +0800

    :mushroom::jack_o_lantern:chore: update dev background, manifest

---
+++ shells/maker/src/devtools-background.js
@@ -0,0 +1,59 @@
+/* eslint-disable no-unused-vars */
+//@flow
+
+// This is the devtools script
+
+let panelLoaded = false;
+let panelShown = false;
+let created = false;
+let checkCount = 0;
+
+chrome.devtools.network.onNavigated.addListener(createPanelIfHasCC);
+const clearCCInterval = setInterval(createPanelIfHasCC, 1000);
+createPanelIfHasCC();
+
+function createPanelIfHasCC() {
+  if (created || checkCount++ > 10) {
+    return;
+  }
+
+  panelLoaded = false;
+  panelShown = false;
+
+  chrome.devtools.inspectedWindow.eval(
+    "!!(window.__POTATO_DEVTOOLS_GLOBAL_HOOK__.cc)",
+    function(hasCC) {
+      if (!hasCC || created) {
+        return;
+      }
+      clearInterval(clearCCInterval);
+      created = true;
+      chrome.devtools.panels.create(
+        "CC",
+        "icons/128.png",
+        "devtools.html",
+        panel => {
+          // panel loaded
+          panel.onShown.addListener(onPanelShown);
+          panel.onHidden.addListener(onPanelHidden);
+        }
+      );
+    }
+  );
+}
+
+chrome.runtime.onMessage.addListener(request => {
+  if (request === "potato-panel-load") {
+    panelLoaded = true;
+  }
+});
+
+function onPanelHidden() {
+  chrome.runtime.sendMessage("potato-panel-hidden");
+  panelShown = false;
+}
+
+function onPanelShown() {
+  chrome.runtime.sendMessage("potato-panel-shown");
+  panelShown = true;
+}
