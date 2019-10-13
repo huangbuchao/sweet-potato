95e7b16e55f4481bc8aa4615f4bdab6b2112afa0
huangbuchao <huangbuchao@wanduoduo.com>
2019-10-13 22:51:22 +0800

    shared: add release support

---
+++ shared/release.js
@@ -0,0 +1,45 @@
+#!/usr/bin/env node
+
+const fs = require("fs");
+const inquirer = require("inquirer");
+const semver = require("semver");
+const pkg = require("../package.json");
+const manifest = require("../shells/chrome/manifest.json");
+const {join} = require("path");
+
+const currentVersion = pkg.version;
+
+(async () => {
+  const { newVersion } = await inquirer.prompt([{
+    type: "input",
+    name: "newVersion",
+    message: `Please provide a version (current: ${currentVersion}):`
+  }]);
+
+  if(!semver.valid(newVersion)) {
+    console.error(`Invalid version: ${newVersion}`);
+    process.exit(1);
+  }
+
+  if (semver.lt(newVersion, currentVersion)) {
+    console.error(`New version (${newVersion}) cannot be lower than current version (${currentVersion}).`)
+    process.exit(1)
+  }
+
+  const { yes } = await inquirer.prompt([{
+    type: "confirm",
+    name: "yes",
+    message: `Release ${newVersion}?`
+  }]);
+
+  if(yes) {
+    pkg.version = newVersion;
+    manifest.version = newVersion;
+    manifest.version_name = newVersion;
+
+    fs.writeFileSync(join(__dirname, "..", "package.json"), JSON.stringify(pkg, null, 2));
+    fs.writeFileSync(join(__dirname, "..", "shells/chrome/manifest.json"), JSON.stringify(manifest, null, 2));
+  }else{
+    process.exit(1);
+  }
+})();
