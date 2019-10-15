1afdcd6dd6cfe379a2d1d87869ca94ebb662928f
huangbuchao <huangbuchao@wanduoduo.com>
2019-10-15 15:15:36 +0800

    add git tag helper

--- git-helper.js
+++ git-helper.js
@@ -1,6 +1,7 @@
 #!/bin/usr/env node
 
 const { execSync } = require("child_process");
+const { version } = require("./package.json");
 
 const args = process.argv.slice(2);
 const commitMessagePrefix = {
@@ -18,6 +19,10 @@ function push(arg1 = "default", ...argRest) {
     execSync("git add .");
     execSync(`git commit -m "${commitMessagePrefix[arg1]}${argRest && argRest.join(" ")}"`);
     execSync("git push");
+    if(arg1 === "release") {
+      execSync(`git tag v${version}`);
+      execSync(`git push origin v${version}`);
+    }
   } catch (error) {
     console.error(error);
   }
